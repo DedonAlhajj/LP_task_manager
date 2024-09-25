dd0d1306902346cee44fbcfbdfdaefb38d1ab5d9
DedonAlhajj <alissar37alhajj@gmail.com>
2024-09-25 18:05:35 +0300

    co1bgggg

---
+++ app/Http/Controllers/DashboradController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class DashboradController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    protected $user;
+    protected $taskRepo;
+
+
+    public function __construct()
+    {
+        Auth::check();
+        $this->user = Auth::user();
+
+    }
+    public function index()
+    {
+        $projects = [];
+
+        if (Auth::user()->hasRole('Admin') || Auth::user()->hasRole('Manager')) {
+            $projects = Project::all();
+
+        }
+        elseif (Auth::user()->hasRole('Team Member')) {
+            //عرض المشاريع التي يكون للمستخدم (Auth) مهام موكلة إليه فيها فقط
+            $u = Auth::user();
+            $projects = $u->projects;
+
+        }
+        //dd($projects);
+        return view('index', compact('projects'));
+    }
+
+
+}
